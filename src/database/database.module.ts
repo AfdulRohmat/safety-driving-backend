fe3dfe89831d79585cf090bdb2d7bd7acb441d87
Afdul Rohmat <afdulrohmat03@gmail.com>
2024-06-06 20:29:39 +0700

    update, ready to deploy

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,6 +7,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
+            // useFactory: async (configService: ConfigService) => ({
+            //     type: 'postgres',
+            //     host: '34.101.213.241',
+            //     port: 5432,
+            //     username: 'postgres',
+            //     password: 'passwordta-safety-driving-2024@',
+            //     database: 'safety-driving-db',
+            //     synchronize: true, // Disable in production
+            //     autoLoadEntities: true,
+            //     entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Adjust entity path if needed
+            // }),
             useFactory: async (configService: ConfigService) => ({
                 type: 'postgres',
                 host: configService.getOrThrow('DATABASE_HOST'),
